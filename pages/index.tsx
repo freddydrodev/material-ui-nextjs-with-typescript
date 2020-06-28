cf5b789201c76dd4a8d9abc4395cbdd5c7ec7c13
drosarl <drosarl.infos@gmail.com>
2020-06-28 18:43:45 +0000

    cleaned

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+
 
 export default function Index() {
   return (
@@ -13,11 +11,6 @@ export default function Index() {
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js with TypeScript example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
